d33e9aea7297371405446ee480fac871e433d753
Zidane <75941907+Zidane404@users.noreply.github.com>
2026-01-19 15:05:58 +0000

    Update Lab01_Java.java
    Added Comment

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,6 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
+  //Edit made by Zidane_230127926
 
 
   //Last line print('Good bye')
@@ -21,3 +22,4 @@ void main()
     testMethod1();
     testMethod2();
 }
+
